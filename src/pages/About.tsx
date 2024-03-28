b34a6142cddf73e89f198444d735281073bf0a5d
amandaalexandre <amandaalexandre@outlook.com>
2024-03-28 04:56:18 -0300

    reset about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -5,8 +5,6 @@ const About: React.FunctionComponent = () => (
   <div className="page-content">
     <h1>Welcome to the About Page</h1>
     <a href={`https://pom.cards/`} target="_blank">Learn more about PoM here</a>
-    <br></br>
-    <b>We are looking for contributors.</b> <a href="https://discord.gg/8GZrhMAwgs" target="_blank">You can join our Discord</a> and get to know how you can contribute here.
   </div>
 );
 
