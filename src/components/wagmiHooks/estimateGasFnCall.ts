6bcab31eb0154c80de9e45fba70b1b2107e16a0d
tracyspacy <42025315+tracyspacy@users.noreply.github.com>
2024-10-28 20:29:36 +0100

    TG-410 community registration est added

--- src/components/wagmiHooks/estimateGasFnCall.ts
+++ src/components/wagmiHooks/estimateGasFnCall.ts
@@ -13,7 +13,10 @@ type fnArgs = AbiParametersToPrimitiveTypes<
 >;
 type fnNames = ExtractAbiFunctionNames<typeof namesRegistryConfig.abi, 'nonpayable'>;
 
-export function useEstimateGasFnCall(fnName: fnNames, inputs: fnArgs) {
+export function useEstimateGasFnCall(fnName: fnNames, inputs: fnArgs, account?: `0x${string}`) {
+    const account_to_use: `0x${string}` = account
+        ? account
+        : '0xF322bce760aDf38a64953447c8bf401bFDa84a05'; // random empty address if user not connected wallet
     const registerNameData = encodeFunctionData({
         abi: namesRegistryConfig.abi,
         functionName: fnName,
@@ -22,6 +25,7 @@ export function useEstimateGasFnCall(fnName: fnNames, inputs: fnArgs) {
 
     // Estimate the gas usage for the transaction
     const { data: gasEstimate } = useEstimateGas({
+        account: account_to_use,
         to: namesRegistryConfig.address,
         data: registerNameData,
     });
