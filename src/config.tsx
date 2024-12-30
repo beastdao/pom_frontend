ffb19ad32cde1408e15df71af88dd3179686bbf8
tracyspacy <42025315+tracyspacy@users.noreply.github.com>
2024-12-30 17:37:09 +0100

    added transport ie rpc provider to config

--- src/config.tsx
+++ src/config.tsx
@@ -1,4 +1,4 @@
-import { createConfig } from '@wagmi/core';
+import { createConfig, http } from '@wagmi/core';
 import { mainnet, sepolia } from '@wagmi/core/chains';
 import { getDefaultConfig } from 'connectkit';
 
@@ -7,12 +7,16 @@ const walletConnectProjectId: string = import.meta.env.VITE_APP_WALLET_CONNECT!;
 const env = import.meta.env.VITE_APP_ENV;
 // Choose which chains you'd like to show
 const chains_to_use = env === 'mainnet' ? mainnet : env === 'sepolia' ? sepolia : sepolia;
-
+const transport = import.meta.env.VITE_APP_TRANSPORT;
 export const config = createConfig(
     getDefaultConfig({
         appName: 'POM',
         walletConnectProjectId,
         chains: [chains_to_use],
+        transports: {
+            [mainnet.id]: http('https://mainnet.infura.io/v3/' + transport),
+            [sepolia.id]: http('https://sepolia.infura.io/v3/' + transport),
+        },
     })
 );
 
